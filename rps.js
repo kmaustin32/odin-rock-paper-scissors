f740f4e1ae7747cc8d05608d9dcf67352f8afe98
Keenan (Ezra) Austin <kmaustin32@gmail.com>
2023-12-25 10:59:48 +0200

    Add basic choice functions

--- rps.js
+++ rps.js
@@ -2,3 +2,28 @@
 
 // Find instructions here: https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
 
+const choices = ['rock', 'paper', 'scissors'];
+
+var computerChoice, 
+    playerChoice;
+
+function getComputerChoice() {
+  computerChoice = choices[Math.floor(Math.random() * 3)]
+  // console.log(`Computer Choice: ${computerChoice};`)
+  return computerChoice;
+};
+
+function getPlayerChoice() {
+  playerChoice = window.prompt("Choose Rock, Paper, or scissors: ");
+  playerChoice = playerChoice.toLowerCase();
+  // console.log(`pre-conditional player: ${playerChoice};`)
+  if (playerChoice != 'rock' && playerChoice != 'paper' && playerChoice != 'scissors') {
+    playerChoice = window.prompt("Pick again");
+    // console.log(`post-conditional player: ${playerChoice};`)
+    return playerChoice;
+  };
+  return playerChoice;
+};
+
+getComputerChoice();
+getPlayerChoice();
